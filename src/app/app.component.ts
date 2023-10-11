e7001bfce2fd8d26303ea6434b3b9d6c0efd6591
Stephen Burns <burns140@purdue.edu>
2023-10-10 21:58:22 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'burns_angular_proj';
+}
